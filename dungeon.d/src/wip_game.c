e503d31637dd748010c253565ad72820e5ec4e8f
Jovan Lanik <jox969@gmail.com>
2022-03-19 22:56:38 +0100

    update

--- dungeon.d/src/wip_game.c
+++ dungeon.d/src/wip_game.c
@@ -3,11 +3,7 @@
 
 // Rendering Functions
 
-#ifdef __STDC_ALLOC_LIB__
-#define __STDC_WANT_LIB_EXT2__ 1
-#else
 #define _POSIX_C_SOURCE 200809L
-#endif
 
 #include <stdlib.h>
 #include <stdarg.h>
--- src/wip_arg.c
+++ src/wip_arg.c
@@ -3,11 +3,7 @@
 
 // Argument Parsing
 
-#ifdef __STDC_ALLOC_LIB__
-#define __STDC_WANT_LIB_EXT2__ 1
-#else
 #define _POSIX_C_SOURCE 200809L
-#endif
 
 #include <stdlib.h>
 #include <string.h>
