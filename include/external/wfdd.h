1ec1cbb8295dae06536e22d04f87da24d65783ca
Jovan Lanik <jox969@gmail.com>
2023-10-30 17:29:38 +0100

    update wfdd

--- include/external/wfdd.h
+++ include/external/wfdd.h
@@ -12,7 +12,21 @@ typedef struct {
 	struct sockaddr_in addr;
 } wfdd_display_t;
 
+/*
+	Initialize a connections to a display.
+	mcastgroup is string containing a multicasst address.
+
+*/
 wfdd_display_t *wfdd_initDisplay(const char *mcastgroup, int port);
+
+/*
+	Send data to display.
+	You must send the correct size or the display will discard it.
+*/
 int wfdd_sendBuffer(wfdd_display_t *display, size_t size, uint8_t *data);
+
+/*
+	Close connection to display.
+*/
 void wfdd_closeDisplay(wfdd_display_t *display);
 
